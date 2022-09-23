4e7fa3e875f492869de6b2a42e3a1d2d91b804c2
Rossidge <therossmatheson@gmail.com>
2022-09-23 07:39:22 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,158 @@
+import axios from "axios";
+import axiosRetry from "axios-retry";
+
+const authStorage = {
+  token: "foobar",
+  expires: new Date(),
+  getExpiresAt: async function () {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        return res(authStorage.expires)
+      }, 50)
+    })
+  },
+  storeToken: async function (token, dt) {
+    return new Promise((res, rej) => {
+      authStorage.token = token
+      authStorage.expires = dt
+      setTimeout(() => {
+        return res(true)
+      }, 50)
+    })
+  },
+  getToken: async function () {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(authStorage.token)
+      }, 50)
+    })
+  }
+}
+
+
+const _myAxiosErrFn = (error) => {
+  if (error.response) {
+    console.log(error.response)
+  } else if (error.request) {
+
+  } else {
+
+  }
+
+  return Promise.reject(error);
+};
+
+const _myAxioSuccessFn = (r) => {
+  console.log("REQUEST SUCCESS INTERCEPTOR:", r.config.url, r.config.data, r.data)
+  return r;
+};
+
+const onRequest = async (config) => {
+  if (config.requires_auth) {
+    console.log("pre request", config.data.request_number)
+    const time = await authStorage.getExpiresAt()
+    console.log(`getting expires at for ${config.url} ${config.data.request_number}`)
+    // Renew the auth token if its expired
+    if (time && time <= new Date()) {
+        console.log(`we need to update the token for ${config.url}, calling ?foo=bar to refresh`)
+        return apiClient({
+          url: "/testApi/token.json",
+          method: 'get',
+          data: {tokenForRequestNumber: config.data.request_number}
+        }).then(async function (res) {
+          const t = res.data.token
+          console.log("new token is received for", config.data.request_number)
+          console.log("waiting for store")
+          await authStorage.storeToken(t, new Date());
+          console.log("token stored async we can return for", config.data.request_number)
+          config.headers.authorization = t;
+          return config;
+        }).catch(function (e) {
+          return config;
+        });
+    } else {
+      return config
+    }
+  } else {
+    return config;
+  }
+}
+
+axios.interceptors.request.use(async (config) => await onRequest(config));
+axios.interceptors.response.use(_myAxioSuccessFn, _myAxiosErrFn);
+
+async function apiClient(requestConfig) {
+  const authToken = await authStorage.getToken();
+
+  const defaultConfig = {
+    baseURL: "./",
+    method: "get",
+    requires_auth: false,
+    headers: {
+      authorization: 'Bearer ' + authToken,
+    }
+  };
+
+  const finalRequestConfig = {
+    ...defaultConfig,
+    ...requestConfig,
+  };
+
+  // axiosRetry(axios, {
+  //   retryDelay: axiosRetry.exponentialDelay,
+  //   retries: 5,
+  //   onRetry: function () {
+  //     console.log('retry');
+  //   },
+  //   retryCondition: function (err) {
+  //     const message = (err && err.message) ? err.message : '';
+  //     const found = ['404', '500'].filter((status) => {
+  //       return message.indexOf(status) !== -1;
+  //     })
+  //     return found.length;
+  //   }
+  // });
+
+  return axios(finalRequestConfig);
+}
+
+// simulated multiple page layer auth requests
+[1,2,3].map((n) => {
+  setTimeout(() => {
+    console.log(`firing request ${n}`)
+    apiClient({
+      url: "/testApi/test.json",
+      requires_auth: true,
+      data: {
+        request_number: n
+      }
+    }).then(res => {
+      console.log(`local res success ${n}`, res)
+    }).catch(err => {
+      console.log(`local catch fail ${n}`, err)
+    })
+  }, 50)
+})
+
+;[4,5,6].map((n) => {
+  setTimeout(() => {
+    console.log(`firing request ${n}`)
+    apiClient({
+      url: "/testApi/test2.json",
+      requires_auth: true,
+      data: {
+        request_number: n
+      }
+    }).then(res => {
+      console.log(`local res success ${n}`, res)
+    }).catch(err => {
+      console.log(`local catch fail ${n}`, err)
+    })
+  }, 175)
+})
+
+
+
+
+
+
